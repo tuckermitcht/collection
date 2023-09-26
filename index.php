a93525713e11928da057b4e99524d7520d987ac8
Mitch <michaelthierytucker@gmail.com>
2023-09-26 10:06:40 +0100

    initial html

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Forms Introduction</title>
+
+    <meta name="description" content="Intro to forms with PHP">
+    <meta name="author" content="iO Academy">
+
+     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+
+</head>
+
+<body>
+
+<h1>My Beer Collection</h1>
+
+
+
+
+</body>
+</html>
